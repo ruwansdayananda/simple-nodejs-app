87a00276cc2144e73a706224f4b44cddd294a887
Steffy Ann John <steffy@qburst.com>
2019-03-04 17:36:51 +0530

    Initializing routes of api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test',(req,res)=>res.json({
-    message:"User Api Works"
+    message:"Post Api Works"
 }))
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose =  require('mongoose');
+const users = require('./routes/api/users');
+const profile =  require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 // DB config
 const mongodb = require('./config/keys').mongoURI;
@@ -9,8 +12,13 @@ mongoose.connect(mongodb)
 .then(()=>console.log("mongdb Connected"))
 .catch((err)=>console.log("Err connecting mongodb:",err))
 
-
 const app = express();
+
+// use routes
+app.use('/api/users',users);
+app.use('/api/posts',posts);
+app.use('/api/profile',profile);
+
 app.get('/',(req,res)=>res.send('Hello world Steffy ann john'));
 
 const port = process.env.PORT || 4000;
